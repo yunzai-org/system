2d256dcb7ca8506f7a30bd52829d0d870f9ef534
ningmengchongshui <916415899@qq.com>
2024-06-28 12:57:42 +0800

    fix: 优化为ts写法

--- apps/update.ts
+++ apps/update.ts
@@ -1,7 +1,7 @@
 import { Plugin, makeForwardMsg } from 'yunzai/core'
 import lodash from 'lodash'
 import { existsSync, readdirSync } from 'node:fs'
-import { BOT_NAME } from 'yunzai/config'
+import { BOT_NAME, PLUGINS_PATH } from 'yunzai/config'
 import { exec, execSync } from 'child_process'
 import { Restart } from './restart.js'
 import { sleep } from 'yunzai/utils'
@@ -13,6 +13,9 @@ let uping = false
 export class update extends Plugin {
   typeName = BOT_NAME
   messages = []
+  isUp = null
+  isNowUp = null
+  oldCommitId = null
   constructor() {
     super()
     this.priority = 4000
@@ -33,46 +36,67 @@ export class update extends Plugin {
     ]
   }
 
+  /**
+   * 
+   * @returns 
+   */
   async update() {
+    // 不是主人
     if (!this.e.isMaster) return false
-    if (uping) return this.e.reply('已有命令更新中..请勿重复操作')
-
+    if (uping) {
+      this.e.reply('正在更新中..请勿重复操作')
+      return false
+    }
+    // 其他指令重合反弹
     if (/详细|详情|面板|面版/.test(this.e.msg)) return false
 
-    /** 获取插件 */
-    let plugin = this.getPlugin()
-    if (plugin === false) return false
+    // 获取插件
+    const name = this.getPlugin()
 
-    /** 执行更新 */
-    if (plugin === '') {
+    // false 错误
+    if (name === false) return false
+
+    // 空的，全部更新
+    if (name === '') {
+      // 执行更新
       await this.runUpdate('')
-      await sleep(1000)
-      plugin = this.getPlugin('miao-plugin')
-      await this.runUpdate(plugin)
     } else {
-      await this.runUpdate(plugin)
+      //
+      await this.runUpdate(name)
     }
 
-    /** 是否需要重启 */
+    //是否需要重启
     if (this.isUp) {
-      // await this.e.reply('即将执行重启，以应用更新')
-      setTimeout(() => this.restart(), 2000)
-    }
-  }
-
-  getPlugin(plugin = '') {
-    if (!plugin) {
-      plugin = this.e.msg.replace(/#(强制)?更新(日志)?/, '')
-      if (!plugin) return ''
+      setTimeout(this.restart, 2000)
     }
 
-    if (!existsSync(`plugins/${plugin}/.git`)) return false
-
-    this.typeName = plugin
-    return plugin
   }
 
-  async execSync(cmd) {
+  /**
+   * 
+   * @param plugin 
+   * @returns 
+   */
+  getPlugin(name = '') {
+    if (!name || name == '') {
+      // 没有设置  - 就捕获指令上的
+      name = this.e.msg.replace(/#(强制)?更新(日志)?/, '')
+      // 不存在
+      if (!name || name == '') return ''
+    }
+    // 指定插件不存在
+    if (!existsSync(`plugins/${name}/.git`)) return false
+    this.typeName = name
+    // 存在则返回
+    return name
+  }
+
+  /**
+   * 
+   * @param cmd 
+   * @returns 
+   */
+  async execSync(cmd: string): Promise<{ error, stdout, stderr }> {
     return new Promise(resolve => {
       exec(cmd, { windowsHide: true }, (error, stdout, stderr) => {
         resolve({ error, stdout, stderr })
@@ -80,31 +104,49 @@ export class update extends Plugin {
     })
   }
 
-  isUp = null
-  isNowUp = null
-  oldCommitId = null
 
+  /**
+   * 
+   * @param plugin 
+   * @returns 
+   */
   async runUpdate(plugin = '') {
+
+    //
     this.isNowUp = false
 
     let cm = 'git pull --no-rebase'
 
     let type = '更新'
+
+    //
     if (this.e.msg.includes('强制')) {
+      //
       type = '强制更新'
       cm = `git reset --hard && git pull --rebase --allow-unrelated-histories`
+
+      //
     }
+    //
     if (plugin) cm = `cd "plugins/${plugin}" && ${cm}`
 
     this.oldCommitId = await this.getcommitId(plugin)
 
+    //
     logger.mark(`${this.e.logFnc} 开始${type}：${this.typeName}`)
 
     await this.e.reply(`开始${type} ${this.typeName}`)
+
+    //
     uping = true
+
+    //
     const ret = await this.execSync(cm)
+
+    //
     uping = false
 
+    //
     if (ret.error) {
       logger.mark(`${this.e.logFnc} 更新失败：${this.typeName}`)
       this.gitErr(ret.error, ret.stdout)
@@ -118,21 +160,33 @@ export class update extends Plugin {
     } else {
       await this.e.reply(`${this.typeName} 更新成功\n更新时间：${time}`)
       this.isUp = true
-      await this.e.reply(await this.getLog(plugin))
+      const msg = await this.getLog(plugin)
+      if (msg) await this.e.reply(msg)
     }
 
     logger.mark(`${this.e.logFnc} 最后更新时间：${time}`)
     return true
   }
 
-  async getcommitId(plugin = '') {
+  /**
+   * 
+   * @param name 
+   * @returns 
+   */
+  async getcommitId(name = '') {
+    //
     let cm = 'git rev-parse --short HEAD'
-    if (plugin) cm = `cd "plugins/${plugin}" && ${cm}`
-
+    //
+    if (name) cm = `cd "plugins/${name}" && ${cm}`
     const commitId = await execSync(cm, { encoding: 'utf-8' })
     return lodash.trim(commitId)
   }
 
+  /**
+   * 
+   * @param plugin 
+   * @returns 
+   */
   async getTime(plugin = '') {
     let cm = 'git log -1 --pretty=%cd --date=format:"%F %T"'
     if (plugin) cm = `cd "plugins/${plugin}" && ${cm}`
@@ -149,6 +203,12 @@ export class update extends Plugin {
     return time
   }
 
+  /**
+   * 
+   * @param err 
+   * @param stdout 
+   * @returns 
+   */
   async gitErr(err, stdout) {
     const msg = '更新失败！'
     const errMsg = err.toString()
@@ -179,18 +239,23 @@ export class update extends Plugin {
     return this.e.reply([errMsg, stdout])
   }
 
+  /**
+   * 
+   */
   async updateAll() {
-    const dirs = readdirSync('./plugins/')
-    const MSG = message => {
-      // 收集
-      this.messages.push(message)
-    }
+    // 得到目录下的所有插件
+    const dirs = readdirSync(`./${PLUGINS_PATH}/`)
+    // 校验
     const testReg = /^#静默全部(强制)?更新$/.test(this.e.msg)
     if (testReg) {
+      // 等待发送
       await this.e.reply(`开始执行静默全部更新,请稍等...`)
     }
+    // 运行更新
     await this.runUpdate()
+    //
     for (const plu of dirs) {
+      // 得到
       const Plu = this.getPlugin(plu)
       if (Plu === false) continue
       await sleep(1500)
@@ -198,11 +263,10 @@ export class update extends Plugin {
     }
     if (testReg) {
       const msg = await makeForwardMsg(this.e, this.messages)
-      MSG(msg)
+      this.messages.push(msg)
     }
     if (this.isUp) {
-      // await this.e.reply('即将执行重启，以应用更新')
-      setTimeout(() => this.restart(), 2000)
+      setTimeout(this.restart, 2000)
     }
   }
 
@@ -217,62 +281,87 @@ export class update extends Plugin {
 
   /**
    * 
-   * @param plugin 
+   * @param name 
    * @returns 
    */
-  async getLog(plugin = '') {
+  async getLog(name = '') {
     let cm = 'git log -100 --pretty="%h||[%cd] %s" --date=format:"%F %T"'
-    if (plugin) cm = `cd "plugins/${plugin}" && ${cm}`
+    if (name) cm = `cd "plugins/${name}" && ${cm}`
 
-    let logAll
+    // 所有记录
+    let logAll: string | null = null
+
+    //
     try {
       logAll = await execSync(cm, { encoding: 'utf-8' })
     } catch (error) {
+      // 不错啦
       logger.error(error.toString())
       await this.e.reply(error.toString())
     }
 
+    // 记录不存在
     if (!logAll) return false
 
-    logAll = logAll.trim().split('\n')
+    const logArray = logAll.trim().split('\n')
 
-    let log = []
-    for (let str of logAll) {
-      str = str.split('||')
-      if (str[0] == this.oldCommitId) break
-      if (str[1].includes('Merge branch')) continue
-      log.push(str[1])
+    const log = []
+    for (let str of logArray) {
+      const strArr = str.split('||')
+      if (strArr[0] == this.oldCommitId) break
+      if (strArr[1].includes('Merge branch')) continue
+      log.push(strArr[1])
     }
-    let line = log.length
-    log = log.join('\n\n')
 
-    if (log.length <= 0) return ''
+    const length = log.length
+
+    const logStr = log.join('\n\n')
+
+    if (logStr.length <= 0) return ''
 
     let end = ''
+
     try {
       cm = 'git config -l'
-      if (plugin) cm = `cd "plugins/${plugin}" && ${cm}`
+      if (name) cm = `cd "plugins/${name}" && ${cm}`
       end = await execSync(cm, { encoding: 'utf-8' })
       end = end
         .match(/remote\..*\.url=.+/g)
         .join('\n\n')
         .replace(/remote\..*\.url=/g, '')
         .replace(/\/\/([^@]+)@/, '//')
+      //
     } catch (error) {
+      //
       logger.error(error.toString())
       await this.e.reply(error.toString())
     }
 
+    /**
+     * 合成转发
+     */
     return makeForwardMsg(
       this.e,
-      [log, end],
-      `${plugin || BOT_NAME} 更新日志，共${line}条`
+      [logStr, end],
+      `${name || BOT_NAME} 更新日志，共${length}条`
     )
   }
 
+  /**
+   * 
+   * @returns 
+   */
   async updateLog() {
-    const plugin = this.getPlugin()
-    if (plugin === false) return false
-    return this.e.reply(await this.getLog(plugin))
+    const name = this.getPlugin()
+    if (name === false) return false
+    // 得到指令插件的记录
+    const msg = await this.getLog(name)
+    // 记录错误
+    if (msg) {
+      this.e.reply(msg)
+    } else {
+      this.e.reply("日志获取失败~")
+    }
+    return
   }
 }
