9db50fcdbfe8a40825c0baee1f39e433d5618a9c
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-07-17 23:56:41 +0800

    feat: 简化

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,31 @@
-import * as main from './apps.js'
-import { Store } from './store.js'
+import * as apps from './apps.js'
 import { Options } from './types.js'
+import { Store } from './store'
+import { applicationOptions } from 'yunzai'
 
-/**
- * 
- */
-class App {
-    // 插件名
-    static stattu = 'system'
+// 存储
+const data = []
 
-    //
-    #data = []
-
-    /**
-     * 这里是用户个性化配置信息
-     * @param config 
-     */
-    constructor(config?: Options) {
-        if (config.reStartKey) Store.RESTART_KEY = config.reStartKey
-        // 个性化配置，代替文件形式
-    }
-
-    /**
-     * 插件创建时
-     * 处理的函数
-     * @param config 
-     */
-    create(config) {
-        // yunzai.congi.js 配置信息
-        
-        // 对应用进行排序
-        for (const key in main) {
-            this.#data.push(new main[key]())
-        }
-    }
-
-    /**
-     * 安装中间件之前的函数
-     */
-    beforeMount(event) {
-        //
-    }
-
-    /**
-     * 安装中间件之上会执行的函数
-     * 也就是响应前处理的函数
-     * @returns 
-     */
-    mounted(event) {
-
-
-        // 把处理好的 this.data 丢给机器人
-        return this.#data
-    }
-
-
-    /**
-     * 每条响应时会都处理的函数
-     * @returns 
-     */
-    response() {
-        //
-    }
-
-    
-    /**
-     * 响应后
-     */
-    afterResponse(){
-        // 完成响应后，处理的函数
-    }
-
-
+export default (config?: Options) => {
+    return applicationOptions({
+        create() {
+            // init
+            if (config?.reStartKey) Store.RESTART_KEY = config?.reStartKey
+            // 对应用进行排序
+            for (const key in apps) {
+                data.push(new apps[key]())
+            }
+        },
+        /**
+         * 安装中间件之上会执行的函数
+         * 也就是响应前处理的函数
+         * @returns 
+         */
+        mounted(event) {
+            // 把处理好的 this.data 丢给机器人
+            return data
+        },
+    })
 }
 
-/**
- * 
- */
-export default (config: Options) => new App(config)
+//
