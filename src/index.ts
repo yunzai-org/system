3c6398d59f85cd17e3752bbb03be652ce0358164
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-08-07 23:46:58 +0800

    feat: 修复消息错乱

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,23 @@ import { Options } from './types/types.js'
 import { Store } from './model/store.js'
 import { Init } from './model/init.js'
 export default (config?: Options) => {
-  // 存储
-  const data = useAppStorage()
   // options
   return applicationOptions({
     create() {
       // init
       if (config?.reStartKey) Store.RESTART_KEY = config?.reStartKey
       if (config?.pm2ConfigDir) Store.PM2_CONFIG_DIR = config?.pm2ConfigDir
-      // 对应用进行排序
-      for (const key in apps) {
-        data.push(new apps[key]())
-      }
       // init
       Init()
     },
-    mounted() {
+    mounted(e) {
+      // 存储
+      const data = useAppStorage()
+      // init
+      for (const key in apps) {
+        data.push(new apps[key](e))
+      }
+      // back
       return data
     }
   })
