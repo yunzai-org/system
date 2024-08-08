b79ac362ff53c88470f4a3cbc2a0ae56b63a1735
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-08-08 18:10:12 +0800

    feat: 优化匹配

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,58 @@
-import { applicationOptions, useAppStorage } from 'yunzai'
+import { Application, applicationOptions, useEvent } from 'yunzai'
 import * as apps from './apps.js'
-import { Options } from './types/types.js'
-import { Store } from './model/store.js'
 import { Init } from './model/init.js'
-export default (config?: Options) => {
+export default () => {
+  const rules: {
+    reg: RegExp | string
+    key: string
+  }[] = []
   // options
   return applicationOptions({
     create() {
-      // init
-      if (config?.reStartKey) Store.RESTART_KEY = config?.reStartKey
-      if (config?.pm2ConfigDir) Store.PM2_CONFIG_DIR = config?.pm2ConfigDir
+      // created
+      for (const key in apps) {
+        // 连接
+        const app: typeof Application.prototype = new apps[key]()
+        // 用  reg 和 key 连接起来。
+        for (const rule of app.rule) {
+          rules.push({
+            reg: rule.reg,
+            key: key
+          })
+        }
+      }
       // init
       Init()
     },
-    mounted(e) {
+    async mounted(e) {
       // 存储
-      const data = useAppStorage()
-      // init
-      for (const key in apps) {
-        data.push(new apps[key](e))
-      }
+      const data = []
+      // 如果key不存在
+      const cache = {}
+      // 使用event以确保得到正常类型
+      await useEvent(
+        e => {
+          for (const item of rules) {
+            // 匹配正则
+            // 存在key
+            // 第一次new
+            if (
+              new RegExp(item.reg).test(e.msg) &&
+              apps[item.key] &&
+              !cache[item.key]
+            ) {
+              cache[item.key] = true
+              data.push(new apps[item.key]())
+            }
+          }
+        },
+        // 推倒为message类型的event
+        [e, 'message']
+      )
       // back
       return data
     }
   })
 }
+
 export * from './apps.js'
